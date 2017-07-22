abf4f744167958eac06299823f5cdde2e5db37fc
Vincent Smuda <vincent@vincentsmuda.com>
2017-07-21 22:34:53 -0400

    adds personal site to resume

--- source/js/partials/components/_resume.js
+++ source/js/partials/components/_resume.js
@@ -10,6 +10,7 @@ module.exports = {
 	_website:  "http://www.vincentsmuda.com",
 	_location: "Montréal, Quebec, Canada",
 	_email:    "throwmail@vincentsmuda.com",
+	_github:	 "https://github.com/thefreshvince",
 
 	education: [
 		{
@@ -19,28 +20,69 @@ module.exports = {
 		}
 	],
 
-	employment: {
-		company:  "Overhaul Media",
-		position: "Developer",
-		period:   "2010-2015",
-		tasks:
-		[ "Worked closely with clients",
-			"Collaborated with team",
-			"Integrated designs",
-			"Built custom solutions" ]
-	},
+	employment: [
+
+		{
+			company:  "Overhaul Media",
+			position: "Developer",
+			period:   "2010-2015",
+			tasks:[
+				"Worked closely with clients",
+				"Collaborated with team",
+				"Integrated designs"
+			]
+		},
+
+		{
+			company:  "Designshopp",
+			position: "Developer",
+			period:   "2016-2017",
+			tasks:[
+				"Built a plethora of websites",
+				"Built tools to help increase productivity",
+				"Introduced some programming best practices"
+			]
+		}
+
+	],
 
 	skills: {
-	development:
-	[ "HTML", "CSS", "Sass", "PHP", "GIT", "Gulp",
-		"JS", "WordPress", "Shopify", "Laravel" ],
 
-	design:
-	[ "Photoshop", "Illustrator", "AfterEffects" ],
+		development:
+		[ "JS", "Webpack", "HTML", "CSS", "Sass", "PHP",
+			"GIT", "Gulp", "Shopify", "Laravel", "WordPress" ],
 
-	other:
-	[ "Project Management", "Communication",
-		"Quality Assurance", "Time Managment",
-		"Collaboration", "Problem Solving" ]
-	}
+		design:
+		[ "Photoshop", "Illustrator", "AfterEffects" ],
+
+	},
+
+	projects: [
+
+		{
+			name: 'Exterminator',
+			description: 'A QA script that saves everybody time.',
+			url: 'https://github.com/thefreshvince/TheExterminator'
+		},
+
+		{
+			name: 'Meal',
+			description: 'A flexible scaffolding tool built with node.',
+			url: 'https://github.com/thefreshvince/meal'
+		},
+
+		{
+			name: 'Extractor',
+			description: 'A script that extracts dist files of production node modules.',
+			url: 'https://github.com/thefreshvince/package-extractor'
+		},
+
+		{
+			name: 'Personal Site',
+			description: 'As I\'m not allowed to show sites that I have worked on, please use my publically published personal site to review my development integrity.',
+			url: 'https://github.com/thefreshvince/vincentsmuda.com'
+		}
+
+	]
+
 };
